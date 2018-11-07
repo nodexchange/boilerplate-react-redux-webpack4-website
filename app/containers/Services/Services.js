e6151be873d2cc24530e548bf95e6bf61345207e
Martin (mac) <nodexchange@outlook.com>
2018-11-07 22:57:02 +0000

    services completed

--- app/containers/Services/Services.js
+++ app/containers/Services/Services.js
@@ -4,6 +4,15 @@ import data from './services-data.json';
 import './Services.scss';
 
 export default class Services extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ animation: 'serviceItemAnimation' });
+    }, 500);
+  }
   render() {
     let services = ['Pitch Support', 'Creative Production', 'Ad Support', 'Design', 'Interactive Mocks', 'Innovation Support', 'Training and Workshops', 'Ad Service', 'Consultancy'];
     let rows = [];;
@@ -11,7 +20,8 @@ export default class Services extends Component {
       let dataPoint = data[services[i]];
       rows.push(
         // <SectionItem inView={false} key={i} order={i} {...frontPanelsCopy[i]} offset={this.props.offsetRatio} />
-        <div key={i} className={'service' + i + ' ' + 'serviceItem'}>
+        <div key={i} className={'service' + i + ' ' + 'serviceItem' + ' ' + this.state.animation}>
+          <i className={'sprite' + ' ' + dataPoint.icon}></i>
           <div className={'serviceHeader'}>{dataPoint.header}</div>
           <div className={'serviceDescription'}>{dataPoint.description}</div>
         </div>
